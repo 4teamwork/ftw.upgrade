1ab6b30b80e24433325a0d140ba77b483c3c2a4b
Jonas Baumann <jone@jone.ch>
2011-11-24 17:15:45 +0100

    Add IUpgradeManager utility interface.

--- ftw/upgrade/interfaces.py
+++ ftw/upgrade/interfaces.py
@@ -1,1 +1,13 @@
 from zope.interface import Interface
+
+
+class IUpgradeManager(Interface):
+    """Utility interface for the upgrade manager.
+    """
+
+    def add_upgrade_directory(path):
+        """Registers a upgrade directory.
+
+        Arguments:
+        `path` -- Absolute path of the directory.
+        """

728de71df08e650904908ee2b4b3cc1e371e3e99
Jonas Baumann <jone@jone.ch>
2011-11-24 23:17:41 +0100

    Add more utils functions.

--- ftw/upgrade/tests/test_utils.py
+++ ftw/upgrade/tests/test_utils.py
@@ -11,3 +11,63 @@ class TestUtils(TestCase):
     def test_get_dotted_name(self):
         self.assertEqual(utils.get_dotted_name(Foo),
                          'ftw.upgrade.tests.test_utils.Foo')
+
+    def test_get_modules(self):
+        dottedname = 'ftw.upgrade.tests.data.bar'
+
+        expected_modules = set((
+            'ftw.upgrade.tests.data.bar',
+            'ftw.upgrade.tests.data.bar.one',
+            'ftw.upgrade.tests.data.bar.one.baz',
+            'ftw.upgrade.tests.data.bar.two',
+            'ftw.upgrade.tests.data.bar.two.three',
+            'ftw.upgrade.tests.data.bar.two.three.foo',
+            ))
+
+        modules = utils.get_modules(dottedname)
+
+        module_names = []
+
+        for module in modules:
+            self.assertEqual(type(module).__name__, 'module')
+            module_names.append(module.__name__)
+
+        self.assertEqual(len(set(module_names)), len(module_names))
+
+        self.assertEqual(set(module_names), expected_modules)
+
+    def test_filepath_to_dottedname(self):
+        self.assertEqual(
+            utils.filepath_to_dottedname('/tmp/foo', '/tmp/foo/bar/baz.py'),
+            'bar.baz')
+
+        self.assertEqual(
+            utils.filepath_to_dottedname('/tmp/foo',
+                                         '/tmp/foo/bar/baz/bla.py',
+                                         prefix='yeah'),
+            'yeah.bar.baz.bla')
+
+        self.assertEqual(
+            utils.filepath_to_dottedname('/tmp/foo',
+                                         '/tmp/foo/bar/__init__.py'),
+            'bar')
+
+        with self.assertRaises(ValueError) as cm:
+            utils.filepath_to_dottedname('/tmp/foo/bar', '/tmp/foo')
+        self.assertEqual(
+            str(cm.exception),
+            '`path` (/tmp/foo) does not begin with `basepath` (/tmp/foo/bar)'
+            )
+
+
+
+# bar
+# bar/__init__.py
+# bar/one
+# bar/one/__init__.py
+# bar/one/baz.py
+# bar/two
+# bar/two/__init__.py
+# bar/two/three
+# bar/two/three/__init__.py
+# bar/two/three/foo.py
--- ftw/upgrade/utils.py
+++ ftw/upgrade/utils.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+
 def get_dotted_name(cls):
     """Returns the dotted name of a class.
 
@@ -5,3 +9,67 @@ def get_dotted_name(cls):
     `cls` -- The class.
     """
     return '.'.join((cls.__module__, cls.__name__))
+
+
+def get_modules(dottedname):
+    package = get_module(dottedname)
+
+    packagename, ext_ = os.path.splitext(os.path.basename(package.__file__))
+    if packagename != '__init__':
+        return [package]
+
+    path = os.path.abspath(os.path.dirname(package.__file__))
+
+    modules = []
+
+    for dirpath, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            basename, ext_ = os.path.splitext(filename)
+            if ext_ not in ('.py', '.pyc', 'pyo'):
+                continue
+
+            filepath = os.path.join(dirpath, filename)
+            dottedname = filepath_to_dottedname(path, filepath,
+                                                prefix=package.__name__)
+            module = get_module(dottedname)
+
+            if module not in modules:
+                modules.append(module)
+
+    return modules
+
+
+def filepath_to_dottedname(basepath, path, prefix=''):
+    """Converts a filename `path` to a dotted name by removing the `basepath`
+    and converting the rest.
+    """
+    path = os.path.normpath(path)
+    basepath = os.path.normpath(basepath)
+
+    if not path.startswith(basepath):
+        raise ValueError(
+            '`path` (%s) does not begin with `basepath` (%s)'% (
+                path, basepath))
+
+    end = path[len(basepath) + 1:]
+
+    basename, ext_ = os.path.splitext(os.path.basename(end))
+    if basename == '__init__':
+        end = os.path.dirname(end)
+    else:
+        end = os.path.dirname(end) + os.sep + basename
+
+    if len(end) == 0:
+        return prefix
+
+    dottedname = end.replace(os.sep, '.')
+    if prefix:
+        return '.'.join((prefix, dottedname))
+    else:
+        return dottedname
+
+
+def get_module(dottedname):
+    __import__(dottedname)
+    return sys.modules[dottedname]
+

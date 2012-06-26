ed875f43ee705b4abc5b204ae770b183e7a33eb8
Jonas Baumann <jone@jone.ch>
2012-06-26 08:53:09 +0200

    CyclicDependencies exception: call superclass with message.

--- ftw/upgrade/exceptions.py
+++ ftw/upgrade/exceptions.py
@@ -1,7 +1,10 @@
 
+
 class CyclicDependencies(Exception):
     """Cyclic dependencies detected.
     """
 
     def __init__(self, dependencies):
+        super(CyclicDependencies, self).__init__(
+            'Cyclic dependencies detected.')
         self.dependencies = dependencies

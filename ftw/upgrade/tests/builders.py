09e06850bd4b156a43e6d673e7af2337ebd489cd
Jonas Baumann <jone@jone.ch>
2014-12-17 18:25:16 +0100

    refactor test_command_utils to use ftw.builder's package builder.

--- ftw/upgrade/tests/builders.py
+++ ftw/upgrade/tests/builders.py
@@ -2,8 +2,6 @@ from ftw.builder import builder_registry
 from ftw.upgrade.directory import scaffold
 import inflection
 import os
-import shlex
-import subprocess
 
 
 UPGRADE_CODE = '''
@@ -149,100 +147,3 @@ class UpgradeStepBuilder(object):
 
 
 builder_registry.register('ftw upgrade step', UpgradeStepBuilder)
-
-
-SETUP_PY_TEMPLATE = """
-from setuptools import setup, find_packages
-
-setup(name='{name}',
-      version='1.0.0.dev0',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages={namespaces},
-      include_package_data=True,
-      zip_safe=False,
-
-      install_requires=[
-        'setuptools',
-      ])
-"""
-
-NAMESPACE_INIT_TEMPLATE = """
-# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
-"""
-
-class PackageBuilder(object):
-
-    def __init__(self, session):
-        self.session = session
-        self.named('test.package')
-        self.build_egginfo = False
-
-    def named(self, name):
-        self.name = name
-        return self
-
-    def with_egginfo(self):
-        self.build_egginfo = True
-        return self
-
-    def within(self, root_path):
-        self.root_path = root_path
-        return self
-
-    def create(self):
-        paths = {}
-        self._create_setup_py(paths)
-        self._create_package(paths)
-        self._create_upgrades_directory(paths)
-
-        if self.build_egginfo:
-            self._build_egginfo()
-        return paths
-
-    def _create_setup_py(self, paths):
-        setup_py_path = os.path.join(self.root_path, 'setup.py')
-        paths.update({'setup.py': setup_py_path})
-
-        with open(setup_py_path, 'w+') as setup:
-            setup.write(SETUP_PY_TEMPLATE.format(
-                    name=self.name,
-                    namespaces=self.name.split('.')[:-1]))
-
-    def _create_package(self, paths):
-        code_directory = os.path.join(self.root_path, self.name.replace('.', '/'))
-        paths.update({'code_directory': code_directory})
-
-        os.makedirs(os.path.join(code_directory))
-
-        path = self.root_path
-        for namespace in self.name.split('.')[:-1]:
-            path = os.path.join(path, namespace)
-            with open(os.path.join(path, '__init__.py'), 'w+') as namespace_file:
-                namespace_file.write(NAMESPACE_INIT_TEMPLATE)
-
-        open(os.path.join(code_directory, '__init__.py'), 'w+').close()
-
-    def _create_upgrades_directory(self, paths):
-        upgrades = os.path.join(paths['code_directory'], 'upgrades')
-        paths.update({'upgrades': upgrades})
-        os.makedirs(upgrades)
-
-    def _build_egginfo(self):
-        buildout = os.path.join(self.root_path, 'bin', 'buildout')
-        if not os.path.exists(buildout):
-            raise Exception('require buildout script at {0} for building egg info.'.format(
-                    buildout))
-
-        command = '{0} setup setup.py egg_info'.format(buildout)
-        proc = subprocess.Popen(shlex.split(command), cwd=self.root_path,
-                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        _, stderrdata = proc.communicate()
-        assert proc.returncode == 0, 'Failed to run {0} :\n{1}'.format(
-            command, stderrdata)
-
-builder_registry.register('package', PackageBuilder)

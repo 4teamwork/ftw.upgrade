a3e09aab7867c771551a85d4af1129ead22fbf6e
Jonas Baumann <jone@jone.ch>
2015-01-11 17:20:28 +0100

    jsonapi: implement execute_upgrades action.

--- ftw/upgrade/browser/jsonapi.py
+++ ftw/upgrade/browser/jsonapi.py
@@ -1,10 +1,43 @@
 from AccessControl.requestmethod import requestmethod
+from contextlib import contextmanager
+from ftw.upgrade.interfaces import IExecutioner
 from ftw.upgrade.interfaces import IUpgradeInformationGatherer
 from operator import itemgetter
 from Products.CMFCore.utils import getToolByName
+from StringIO import StringIO
 from zope.component import getAdapter
 from zope.publisher.browser import BrowserView
 import json
+import logging
+
+
+REQUIRED = object()
+
+
+class UpgradeNotFound(Exception):
+    def __init__(self, api_upgrade_id):
+        super(UpgradeNotFound, self).__init__(api_upgrade_id)
+        self.api_upgrade_id = api_upgrade_id
+
+
+@contextmanager
+def capture_log():
+    stream = StringIO()
+    handler = logging.StreamHandler(stream)
+    formatter = logging.root.handlers[-1].formatter
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+
+    original_level = logging.root.getEffectiveLevel()
+    logging.root.addHandler(handler)
+    logging.root.setLevel(logging.INFO)
+    try:
+        yield stream
+
+    finally:
+        stream.seek(0)
+        logging.root.removeHandler(handler)
+        logging.root.setLevel(original_level)
 
 
 class PloneSiteAPI(BrowserView):
@@ -15,10 +48,16 @@ class PloneSiteAPI(BrowserView):
         self.gatherer = IUpgradeInformationGatherer(self.portal_setup)
 
     @requestmethod('GET')
-    def get_profile(self, profileid, REQUEST=None):
+    def get_profile(self, profileid=REQUIRED, REQUEST=None):
         """Returns a JSON-encoded dict representation of the Generic Setup
         profile with the given ``id``.
         """
+        if profileid is REQUIRED:
+            return self._error(
+                400, 'Missing "profileid" param in request.',
+                'The "profileid" param is required for this action.'
+                ' The value is expected to be a profile ID,'
+                ' e.g. "my.package:default".')
 
         return self._pretty_json(
             self._refine_profile_info(
@@ -42,6 +81,41 @@ class PloneSiteAPI(BrowserView):
         return self._pretty_json(
             map(self._refine_profile_info,
                 self._get_profiles_proposing_upgrades()))
+
+    @requestmethod('POST')
+    def execute_upgrades(self, upgrades=REQUIRED, REQUEST=None):
+        """Execute a list of upgrades, each identified by the upgrade ID
+        returned by the profile listing actions
+        (``[dest-version]@[profile ID]``).
+        """
+        portal_migration = getToolByName(self.context, 'portal_migration')
+        if portal_migration.needUpgrading():
+            return self._error(
+                400, 'Plone site outdated',
+                'The Plone site is outdated and needs to be upgraded'
+                ' first using the regular Plone upgrading tools.')
+
+        if upgrades is REQUIRED:
+            return self._error(
+                400, 'Missing "upgrades:list" param in request.',
+                'The "upgrades:list" param is required for this action.'
+                ' It should be used once for each upgrade to execute and'
+                ' contain the upgrade ID returned by the API,'
+                ' e.g. "4023@my.package:default".')
+
+        try:
+            data = self._prepare_executioner_data_by_upgrade_ids(upgrades)
+        except UpgradeNotFound, exc:
+            return self._error(
+                400, 'Upgrade ID not found',
+                'The upgrade ID "{0}" could not be found.'.format(
+                    exc.api_upgrade_id))
+
+        executioner = IExecutioner(self.portal_setup)
+        with capture_log() as stream:
+            executioner.install(data)
+
+        return stream.getvalue()
 
     def _refine_profile_info(self, profile):
         return {'id': profile['id'],
@@ -81,5 +155,52 @@ class PloneSiteAPI(BrowserView):
                                          profileinfo['upgrades'])
         return profileinfo
 
+    def _prepare_executioner_data_by_upgrade_ids(self, api_upgrade_ids):
+        gathered_profiles = self.gatherer.get_upgrades()
+
+        # order api_upgrade_ids by gathered_profiles order
+        ordered_upgrade_ids = []
+        for profile in gathered_profiles:
+            for upgrade in profile['upgrades']:
+                ordered_upgrade_ids.append('{0}@{1}'.format(upgrade['sdest'],
+                                                            profile['id']))
+
+        not_found = set(api_upgrade_ids) - set(ordered_upgrade_ids)
+        if not_found:
+            raise UpgradeNotFound(tuple(not_found)[0])
+
+        api_upgrade_ids.sort(key=ordered_upgrade_ids.index)
+
+        # prepare data as accepted by executioner
+        executioner_data = []
+        for api_id in api_upgrade_ids:
+            for upgrade in self._get_upgrades_by_api_id(api_id,
+                                                        gathered_profiles):
+                executioner_data.append((upgrade['profile'], [upgrade['id']]))
+
+        return executioner_data
+
+    def _get_upgrades_by_api_id(self, api_upgrade_id, profiles=None):
+        if not profiles:
+            profiles = self.gatherer.get_upgrades()
+        profiles_map = dict([(profile['id'], profile) for profile in profiles])
+
+        upgrade_sdest, profile_id = api_upgrade_id.split('@')
+        if profile_id not in profiles_map:
+            raise UpgradeNotFound(api_upgrade_id)
+
+        upgrades = [upgrade for upgrade in profiles_map[profile_id]['upgrades']
+                    if upgrade['sdest'] == upgrade_sdest]
+        if len(upgrades) == 0:
+            raise UpgradeNotFound(api_upgrade_id)
+
+        for upgrade in upgrades:
+            upgrade['profile'] = profile_id
+        return upgrades
+
     def _pretty_json(self, data):
         return json.dumps(data, indent=4)
+
+    def _error(self, status, message, details=''):
+        self.request.response.setStatus(status, message)
+        return json.dumps(['ERROR', message, details])
--- ftw/upgrade/tests/test_jsonapi.py
+++ ftw/upgrade/tests/test_jsonapi.py
@@ -1,11 +1,18 @@
+from contextlib import contextmanager
 from datetime import datetime
 from ftw.builder import Builder
 from ftw.testbrowser import browser
 from ftw.testbrowser import browsing
+from ftw.upgrade.directory import scaffold
+from ftw.upgrade.interfaces import IUpgradeStepRecorder
 from ftw.upgrade.tests.base import UpgradeTestCase
 from plone.app.testing import SITE_OWNER_NAME
-from ZPublisher import BadRequest
+from Products.CMFCore.utils import getToolByName
+from urllib2 import HTTPError
+from zope.component import getMultiAdapter
 import json
+import re
+import transaction
 import urllib
 
 
@@ -22,7 +29,7 @@ class TestPloneSiteJsonApi(UpgradeTestCase):
             self.install_profile('the.package:default', version='1')
             self.clear_recorded_upgrades('the.package:default')
 
-            self.api_request('GET', 'get_profile', profileid='the.package:default')
+            self.api_request('GET', 'get_profile', dict(profileid='the.package:default'))
 
             self.assert_json_equal(
                 {'id': 'the.package:default',
@@ -56,12 +63,9 @@ class TestPloneSiteJsonApi(UpgradeTestCase):
 
     @browsing
     def test_get_profile_requires_profileid(self, browser):
-        with self.assertRaises(BadRequest) as cm:
+        with self.expect_api_error(status=400,
+                                   message='Missing "profileid" param in request.'):
             self.api_request('GET', 'get_profile')
-
-        self.assertIn('The parameter, <em>profileid</em>,'
-                      ' was omitted from the request.',
-                      str(cm.exception))
 
     @browsing
     def test_list_profiles(self, browser):
@@ -147,6 +151,58 @@ class TestPloneSiteJsonApi(UpgradeTestCase):
                         ]},
                 browser.json)
 
+    @browsing
+    def test_execute_upgrades_installs_upgrades_in_gatherer_order(self, browser):
+        self.package.with_profile(
+            Builder('genericsetup profile')
+            .with_upgrade(Builder('ftw upgrade step').to(datetime(2011, 1, 1))
+                          .named('The first upgrade step'))
+            .with_upgrade(Builder('ftw upgrade step').to(datetime(2012, 2, 2))
+                          .named('The second upgrade step')))
+
+        with self.package_created():
+            self.install_profile('the.package:default', version='1')
+            self.clear_recorded_upgrades('the.package:default')
+            self.assertFalse(self.is_installed('the.package:default', datetime(2011, 1, 1)))
+            self.assertFalse(self.is_installed('the.package:default', datetime(2012, 2, 2)))
+
+            self.api_request('POST', 'execute_upgrades', (
+                    ('upgrades:list', '20120202000000@the.package:default'),
+                    ('upgrades:list', '20110101000000@the.package:default')))
+            self.assertTrue(self.is_installed('the.package:default', datetime(2011, 1, 1)))
+            self.assertTrue(self.is_installed('the.package:default', datetime(2012, 2, 2)))
+            self.assertEqual(
+                ['UPGRADE STEP the.package:default: The first upgrade step.',
+                 'UPGRADE STEP the.package:default: The second upgrade step.'],
+                re.findall(r'UPGRADE STEP.*', browser.contents))
+
+    @browsing
+    def test_execute_upgrades_requires_upgrades_param(self, browser):
+        with self.expect_api_error(status=400,
+                                   message='Missing "upgrades:list" param in request.'):
+            self.api_request('POST', 'execute_upgrades', {'enforce': 'post'})
+
+    @browsing
+    def test_execute_upgrades_validates_upgrade_ids(self, browser):
+        with self.expect_api_error(status=400,
+                                   message='Upgrade ID not found',
+                                   details='The upgrade ID \"foo@bar:default\"'
+                                   ' could not be found.'):
+            self.api_request('POST', 'execute_upgrades', {'upgrades:list': 'foo@bar:default'})
+
+    @browsing
+    def test_execute_upgrades_not_allowed_when_plone_outdated(self, browser):
+        portal_migration = getToolByName(self.layer['portal'], 'portal_migration')
+        portal_migration.setInstanceVersion('1.0.0')
+        transaction.commit()
+
+        with self.expect_api_error(status=400,
+                                   message='Plone site outdated',
+                                   details='The Plone site is outdated and needs to'
+                                   ' be upgraded first using the regular Plone'
+                                   ' upgrading tools.'):
+            self.api_request('POST', 'execute_upgrades', {'enforce': 'post'})
+
     def assert_json_equal(self, expected, got, msg=None):
         expected = json.dumps(expected, sort_keys=True, indent=4)
         got = json.dumps(got, sort_keys=True, indent=4)
@@ -160,7 +216,11 @@ class TestPloneSiteJsonApi(UpgradeTestCase):
                       'assert_json_contains_profile: expected profile not in JSON')
         self.assert_json_equal(expected_profileinfo, got_profiles[profileid], msg)
 
-    def api_request(self, method, action, **data):
+    def is_installed(self, profileid, dest_time):
+        recorder = getMultiAdapter((self.portal, profileid), IUpgradeStepRecorder)
+        return recorder.is_installed(dest_time.strftime(scaffold.DATETIME_FORMAT))
+
+    def api_request(self, method, action, data=()):
         browser.login(SITE_OWNER_NAME)
 
         if method.lower() == 'get':
@@ -174,3 +234,45 @@ class TestPloneSiteJsonApi(UpgradeTestCase):
 
         else:
             raise Exception('Unsupported request method {0}'.format(method))
+
+    @contextmanager
+    def expect_api_error(self, **expectations):
+        api_error_info = {}
+        with self.expect_request_error() as response_info:
+            yield api_error_info
+
+        api_error_info.update(response_info)
+        del api_error_info['headers']  # not serializable
+        api_error_info['response_message'] = response_info['message']
+        try:
+            body_json = json.loads(response_info['body'])
+            assert len(body_json) == 3
+            assert body_json[0] == 'ERROR'
+            api_error_info['message'] = body_json[1]
+            api_error_info['details'] = body_json[2]
+        except:
+            raise AssertionError(
+                'Unexpected error response body. A three item list is expected,'
+                ' consisting of "ERROR", the error message (short) and the error details.\n'
+                'Response body: {0}'.format(response_info['body']))
+
+        self.assertDictContainsSubset(
+            expectations, api_error_info,
+            'Unexpected error response details.\n\n'
+            'Expected:' +
+            json.dumps(expectations, sort_keys=True, indent=4) +
+            '\nto be included in:\n' +
+            json.dumps(api_error_info, sort_keys=True, indent=4))
+
+    @contextmanager
+    def expect_request_error(self):
+        response_info = {}
+        with self.assertRaises(HTTPError) as cm:
+            yield response_info
+
+        exc = cm.exception
+        response_info['status'] = exc.wrapped.code
+        response_info['message'] = exc.wrapped.msg
+        response_info['url'] = exc.wrapped._url
+        response_info['headers'] = exc.hdrs
+        response_info['body'] = exc.wrapped.read()

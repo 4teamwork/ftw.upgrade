67ed174a2c21e5420efa17736b66ab4000f9a256
Neil Ferreira <git@neilf.net>
2016-09-02 11:14:00 +0800

    Allow ftw.upgrade's control panel to work with jQuery 1.9+

--- ftw/upgrade/browser/resources/manage.js
+++ ftw/upgrade/browser/resources/manage.js
@@ -1,23 +1,23 @@
-(function($) {
-
-    $('.select-none').live('click', function(e) {
+$(document).ready(function() {
+    // Clear all checkboxes when "Select None" is clicked
+    $('.select-none').click(function(e) {
         e.preventDefault();
-        $('.profiles input[type=checkbox]').attr('checked', false);
+        $('.profiles input[type=checkbox]').prop('checked', false);
     });
 
-    $('.select-not-installed').live('click', function(e) {
+    // Select all addons yet to be installd
+    $('.select-not-installed').click(function(e) {
         e.preventDefault();
-        $('.profiles input[type=checkbox]').attr('checked', false);
-        $('.profiles .upgrade.proposed input[type=checkbox]').attr(
-            'checked', true);
+        $('.profiles input[type=checkbox]').prop('checked', false);
+        $('.profiles .upgrade.proposed input[type=checkbox]').prop('checked', true);
     });
 
-    $('.profile-title strong').live('click', function(e){
+    // Show/hide addon's completed upgrades
+    $('.profiles .profile-title strong').click(function(e){
         e.preventDefault();
         var profile = $(this).parents('.profile:first');
         var to_visible = profile.hasClass('hide-done');
         profile.toggleClass('hide-done');
-
         profile.find('.upgrade.done').each(function() {
             if(to_visible) {
                 $(this).show();
@@ -27,15 +27,17 @@
         });
     });
 
-    $('.upgrade').live('click', function(e) {
+    // Check checkbox for upgrade step if upgrade name is clicked
+    $('.profiles .upgrade').click(function(e) {
         if($(e.target).is('input[type=checkbox]')) {
             return;
         }
         var checkbox = $('input[type=checkbox]', $(this));
-        checkbox.attr('checked', checkbox.attr('checked') ? '' : 'checked');
+        checkbox.prop('checked', !checkbox.prop('checked'));
     });
 
-    $('#upgrade-form').live('submit', function(e) {
+    // Hide the upgrade form while an upgrade is being installed
+    $('#upgrade-form').submit(function(e) {
         $(this).hide();
         $('#back-to-upgrades').show();
 
@@ -47,19 +49,17 @@
         }
     });
 
-    $('#back-to-upgrades').live('click', function(e) {
+    // Allow user to return to the upgrades form after installing an upgrade
+    $('#back-to-upgrades').click(function(e) {
         location.reload();
     });
 
-    $(document).ready(function() {
-        $('[data-human-readable-version]').each(function() {
-            $(this).data('original-version', $(this).text());
-            $(this).text($(this).data('human-readable-version'));
-        }).on('mouseenter', function(event) {
-            $(this).text($(this).data('original-version'));
-        }).on('mouseleave', function(event) {
-            $(this).text($(this).data('human-readable-version'));
-        });
+    $('[data-human-readable-version]').each(function() {
+        $(this).data('original-version', $(this).text());
+        $(this).text($(this).data('human-readable-version'));
+    }).on('mouseenter', function(event) {
+        $(this).text($(this).data('original-version'));
+    }).on('mouseleave', function(event) {
+        $(this).text($(this).data('human-readable-version'));
     });
-
-})(jQuery);
+});

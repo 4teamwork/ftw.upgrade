dded17b1b6e392dc9777cf678cae6c0ad825e6ce
Jonas Baumann <jone@jone.ch>
2017-04-19 14:39:36 +0200

    Add support for Plone 5.1.

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(name='ftw.upgrade',
       classifiers=[
         'Framework :: Plone',
         'Framework :: Plone :: 4.3',
+        'Framework :: Plone :: 5.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Programming Language :: Python',

405384c126d031351a0593cf5cee861e9ef43205
Niklaus Johner <niklaus.johner@4teamwork.ch>
2021-09-07 13:10:52 +0200

    Avoid executing same tests twice.
    When importing a test class, it will get executed, so that all
    tests from TestUpgradeStep were executed twice.

--- ftw/upgrade/tests/test_upgrade_step_intids.py
+++ ftw/upgrade/tests/test_upgrade_step_intids.py
@@ -1,7 +1,7 @@
 from ftw.upgrade.testing import INTID_UPGRADE_FUNCTIONAL_TESTING
-from ftw.upgrade.tests.test_upgrade_step import TestUpgradeStep
+from ftw.upgrade.tests import test_upgrade_step
 
 
-class TestUpgradeStepIntids(TestUpgradeStep):
+class TestUpgradeStepIntids(test_upgrade_step.TestUpgradeStep):
 
     layer = INTID_UPGRADE_FUNCTIONAL_TESTING
